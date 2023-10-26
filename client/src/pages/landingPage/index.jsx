ff468b60afc90d15c08826119d8454db3918ebdc
madays <maday.choque@gmail.com>
2023-10-25 21:02:03 -0500

    conexion front - back desde landing page

--- client/src/pages/landingPage/index.jsx
+++ client/src/pages/landingPage/index.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
 import FavoriteEvents from './FavoriteEvents'
 import FavoriteOrganizers from './FavoriteOrganizers'
 import FindEvent from './FindEvent'
 import NextEventYourZone from './NextEventYourZone'
 import UserLocation from "./UserLocation"
+import axios from 'axios';
 
 function LandingPage() {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    // Make a GET request to your server running at http://localhost:3001
+    axios.get('https://localhost:3001/Eventos')
+      .then((response) => {
+        // Handle the data from the server's response
+        setData(response.data);
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the request
+        console.error('Error:', error);
+      });
+  },[]);
+  console.log('data de eventos',data)
   return (
     <>
       <header>
