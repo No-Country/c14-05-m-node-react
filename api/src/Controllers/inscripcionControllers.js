9e808742b2c4c2cad16a09e7293985e605e99efc
Diego Castillo <92400440+Dac1977@users.noreply.github.com>
2023-10-29 23:49:57 -0300

    Arreglada la inscripcion de usuarios a eventos

--- api/src/Controllers/inscripcionControllers.js
+++ api/src/Controllers/inscripcionControllers.js
@@ -1,20 +1,28 @@
 const { Inscripcion, user, Eventos } = require("../db");
 
-const createInscripcion = async (
-  estaInscripto, 
-  isActive, 
-  userid, 
-  eventoid
-  ) => {
+const createInscripcion = async (estaInscripto, isActive, userid, eventoid) => {
   console.log(userid);
   console.log(eventoid);
   const userFound = await user.findByPk(userid);
-  if (userFound) {
+  const eventFound = await Eventos.findByPk(eventoid);
+  if (userFound && eventFound) {
+    const existingInscripcion = await Inscripcion.findOne({
+      where: {
+        userId: userid,
+        EventoId: eventoid,
+      },
+    });
+
+    if (existingInscripcion) {
+      // El usuario ya está inscrito en el evento, mostrar mensaje de error
+      return "El usuario ya está inscripto en este evento";
+    }
     const nuevaInscripcion = await Inscripcion.create({
       estaInscripto,
       isActive,
     });
     await nuevaInscripcion.setUser(userFound);
+    await nuevaInscripcion.setEvento(eventFound);
     return nuevaInscripcion;
   } else {
     return null;
@@ -24,5 +32,3 @@ const createInscripcion = async (
 module.exports = {
   createInscripcion,
 };
-
-
