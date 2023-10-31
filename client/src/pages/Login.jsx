998dc0b08e8930ebfdf14a92d4e9c2922a69f8d3
Federico Acosta <fede.acosta2396@gmail.com>
2023-10-31 14:03:34 -0300

    cambios para responsive

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -65,15 +65,15 @@ function Login() {
 
   return (
     <>
-      {currentUser?.email ? (
-        <LoggedUserPage currentUser={currentUser} />
-      ) : (
-        <div className="container mt-52 flex flex-col items-center font-nunito">
+      <div className="xl:flex xl:items-center xl:justify-center">
+        <div className="container mt-52 flex flex-col items-center font-nunito 2xl:max-w-4xl">
           <form
             onSubmit={formik.handleSubmit}
             className="relative flex  w-full flex-col"
           >
-            <h1 className="mb-4 text-left text-xl  ">Iniciar sesión</h1>
+            <h1 className="mb-4 text-left text-xl font-semibold not-italic leading-8 tracking-[0.15px] 2xl:mb-20 2xl:text-center 2xl:text-4xl 2xl:leading-10 2xl:tracking-[0.25px] ">
+              Iniciar sesión
+            </h1>
             <InputAuth
               type="email"
               name="email"
@@ -108,7 +108,7 @@ function Login() {
               />
 
               <span
-                className="absolute bottom-[13px] right-4 cursor-pointer "
+                className="2xl: absolute bottom-[13px] right-4 cursor-pointer "
                 onClick={tooglePassword}
               >
                 <img
@@ -122,45 +122,47 @@ function Login() {
               htmlFor="password"
               error={formik.touched.password && formik.errors.password}
             ></LabelAuth>
-            <button
-              className="btn-primary btn-md mb-16 mt-8 flex items-center justify-center rounded-[15px] p-4"
-              type="submit"
-            >
-              <h1 className="text-center text-sm text-white ">
-                Iniciar sesión
-              </h1>
-            </button>
+            <div className="flex items-center justify-center">
+              <button
+                className="btn-primary btn-md mb-16 mt-8 flex items-center justify-center rounded-[15px] p-4 2xl:h-16 2xl:w-72 2xl:rounded-none"
+                type="submit"
+              >
+                <h1 className="text-center text-sm text-white 2xl:text-base ">
+                  Iniciar sesión
+                </h1>
+              </button>
+            </div>
           </form>
           {isError ? (
-            <span className="text center text-error">
+            <span className="text center text-error 2xl:text-lg">
               <h1>Email o Contraseña incorrecto</h1>
             </span>
           ) : (
             ""
           )}
 
-          <h2 className="text-center text-base font-normal text-dark ">
+          <h2 className="text-center text-base font-normal text-dark xl:mb-8 2xl:text-lg ">
             ¿No tenés cuenta?
             <Link to={"../signup"} className="ml-1 font-bold">
               Registrate
             </Link>
             .
           </h2>
-          <div className="mb-8 mt-4 h-[1px] w-72 bg-grayB"></div>
+          <div className="mb-8 mt-4 h-[1px] w-72 bg-grayB xl:hidden "></div>
           <button
-            className="hover:opacity-85 mb-5  flex h-12 w-[90%] items-center justify-center gap-4 rounded-[14px] border border-dark p-[10px] outline-none active:bg-gray-200 active:opacity-90"
+            className="hover:opacity-85 mb-5 flex h-12  w-[90%] items-center justify-center gap-4 rounded-[14px] border border-dark p-[10px] outline-none active:bg-gray-200 active:opacity-90 2xl:h-14 2xl:rounded-none"
             onClick={signWithGoogle}
           >
             <img src="/IconoGoogle.svg" alt="google icon" />
             <p>Iniciar sesión con Google</p>
           </button>
-          <button className="hover:opacity-85 mb-5  flex h-12 w-[90%] items-center justify-center gap-4 rounded-[14px] border border-dark p-[10px] outline-none active:bg-gray-200 active:opacity-90">
+          <button className="hover:opacity-85 mb-5 flex h-12  w-[90%] items-center justify-center gap-4 rounded-[14px] border border-dark p-[10px] outline-none active:bg-gray-200 active:opacity-90 2xl:h-14 2xl:rounded-none">
             <img src="/iconoFacebook.svg" alt="facebook icon" />
 
             <p>Iniciar sesión con Facebook</p>
           </button>
         </div>
-      )}
+      </div>
     </>
   );
 }
