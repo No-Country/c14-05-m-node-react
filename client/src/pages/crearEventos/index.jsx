993ea483eb5dcf42c28c97e9df657428c55bc153
Federico Acosta <fede.acosta2396@gmail.com>
2023-11-02 21:42:15 -0300

    fix card

--- client/src/pages/crearEventos/index.jsx
+++ client/src/pages/crearEventos/index.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import CardHorizontal from "../../components/CardHorizontal";
 import Navbar from "../../components/Navbar";
 import NavbarDesktop from "../../components/NavbarDesktopSinSearcher";
 import { UserContext } from "../../components/UserProvider";
